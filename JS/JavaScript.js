bd8e667f28cc6c6a2f11cb9b1aedf048f4a85278
zakharov-ievgen <zakharov.ievgen.ukr@gmail.com>
2022-07-03 17:29:13 +0300

    no link to icon

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -2,12 +2,14 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=
     .then((response) => response.json())
     .then((data) => {
         document.querySelector(".city").innerHTML = "City" + " is " + JSON.stringify(data.name);
-        document.querySelector(".temp").innerHTML = "Temperature" + " is " + JSON.stringify(data.main.temp);
-        document.querySelector(".pressure").innerHTML = "Pressure" + " is " + JSON.stringify(data.main.pressure);
-        document.querySelector(".humidity").innerHTML = "Humiditi" + " is " + JSON.stringify(data.main.humidity);
-        document.querySelector(".speed").innerHTML = "Wind speed" + " is " + JSON.stringify(data.wind.speed);
+        document.querySelector(".temp").innerHTML = "Temperature" + " is " + JSON.stringify(data.main.temp) + " C ";
+        document.querySelector(".pressure").innerHTML =
+            "Pressure" + " is " + JSON.stringify(data.main.pressure) + " hPa ";
+        document.querySelector(".humidity").innerHTML =
+            "Humiditi" + " is " + JSON.stringify(data.main.humidity) + " % ";
+        document.querySelector(".speed").innerHTML = "Wind speed" + " is " + JSON.stringify(data.wind.speed) + " Km/h ";
         document.querySelector(".deg").innerHTML = "Wind deg" + " is " + JSON.stringify(data.wind.deg);
         document.querySelector(".description").innerHTML =
             "Description" + " : " + JSON.stringify(data.weather[0].description);
-        document.querySelector(".js-img").src = data.weather[3].icon;
+        document.querySelector(".js-img").src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     });
